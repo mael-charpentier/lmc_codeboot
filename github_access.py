e68d37c1323346e9656aee5765842c2643523147
MaelC001 <110736025+mael-charpentier@users.noreply.github.com>
2025-06-10 11:30:40 -0400

    add html download

--- github_access.py
+++ github_access.py
@@ -6,9 +6,10 @@ show_file('github_access.py', False)
 
 # download the code from github
 write_file('lmc.py', read_file('https://raw.githubusercontent.com/mael-charpentier/lmc_codeboot/refs/heads/main/lmc.py'))
+write_file('lmc.html', read_file('https://raw.githubusercontent.com/mael-charpentier/lmc_codeboot/refs/heads/main/lmc.html'))
 
 # open the code in an editor
-show_file('lmc.py', False)
+show_file('lmc.py', True)
 
 # run the code
 import lmc
